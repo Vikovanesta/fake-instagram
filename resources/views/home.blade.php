5595ed5c0d0ff4539a02c093e7a9616eb91cc255
Vikovanesta <Vikovanesta@gmail.com>
2023-08-20 20:37:55 +0700

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/img/kokopanda.jpg" width="200px" height="auto" class="rounded-circle" alt="">
+        </div>
+        <div class="col-9 ps-5 pt-5">
+            <div><h1>Hailpanda</h1></div>
+            <div class="d-flex gap-4">
+                <div><strong>153</strong> posts</div>
+                <div><strong>10k</strong> followers</div>
+                <div><strong>101</strong> following</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
