15e34041b2328d0c99b155e39d3ca7b28a1f303e
Vikovanesta <Vikovanesta@gmail.com>
2023-08-21 18:38:36 +0700

    make profile model

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <img src="/img/kokopanda.jpg" width="200px" height="auto" class="rounded-circle" alt="">
         </div>
         <div class="col-9 ps-5 pt-5">
-            <div><h1>Hailpanda</h1></div>
+            <div><h1>{{ $user->username }}</h1></div>
             <div class="d-flex gap-4">
                 <div><strong>153</strong> posts</div>
                 <div><strong>10k</strong> followers</div>
